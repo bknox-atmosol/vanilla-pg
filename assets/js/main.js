91321436ab00dabdaded1fa8e0cce0b24448057c
bknox-atmosol <bknox@atmosol.com>
2014-10-20 16:26:56 -0700

    Update CSS and JavaScript for first version.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,30 @@
     return el;
   };
 
+  var closeBox = function(e){
+    var overlay = body.querySelector('.overlay');
+    overlay.parentNode.removeChild(overlay);
+    var box = body.querySelector('.box');
+    box.parentNode.removeChild(box);
+  };
+
   var pgClick = function(e) {
+    var el = e.target || e.srcElement;
     if(body.querySelector('.overlay') === null) {
       var overlay = document.createElement('div');
       body.appendChild(addClass(overlay,'overlay'));
+      addClick(overlay, closeBox);
     }
+    var box = addClass(document.createElement('div'), 'box');
+    var button  = document.createElement('button');
+    button.innerHTML = '&times;';
+    addClick(button, closeBox);
+    box.innerHTML = '<p>'+el.innerHTML+'</p>';
+    box.appendChild(addClass(button, 'close'));
+    body.appendChild(box);
   };
 
-  var addCkick = function(el, callback) {
+  var addClick = function(el, callback) {
     if(el.addEventListener) {
       el.addEventListener("click", callback,false);
      } else {
